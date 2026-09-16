438441d20b04f33e8a5734e359396a787667b84e
Luminight <Luminight@luminight.dev>
2026-09-16 21:56:31 +0200

    Release 1.1.0

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -37,11 +37,12 @@
 (fn_call (identifier) @function)
 (param_item (identifier) @variable.parameter)
 
-(struct_init (identifier) @constructor)
+(struct_init (path) @constructor)
 (object_field (identifier) @property)
 (struct_init_field (identifier) @property)
 
 (typeident) @type
+(typeident (path) @type)
 (typeident_any) @type.builtin
 (typeident_primitive) @type.builtin
 "void" @type.builtin
@@ -49,12 +50,18 @@
 (number) @constant.builtin
 (bool) @constant.builtin
 (null) @constant.builtin
+(primary (path) @constant.enum)
+(match_arm_cond (path) @constant.enum)
+(constant) @constant
 
 (string_escape) @escape
 (char_escape) @escape
+(hexa_escape) @hexa_escape
 
 "*" @operator
 "&" @operator
+"|" @operator
+"^" @operator
 "=>" @operator
 "=" @operator
 "+=" @operator
@@ -76,6 +83,11 @@
 "%" @operator
 "as" @operator
 "!" @operator
+"|=" @operator
+"&=" @operator
+"^=" @operator
+">>=" @operator
+"<<=" @operator
 
 (char) @string
 (string) @string
